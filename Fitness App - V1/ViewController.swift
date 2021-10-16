07b244e1e5f6491d42b2d8658da37d5b4f0b8a76
Sumana Sudarshan <sudarshansumana@gmail.com>
2021-10-15 23:37:42 -0400

    V1

--- Fitness App - V1/ViewController.swift
+++ Fitness App - V1/ViewController.swift
@@ -8,12 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var inputTextField: UITextField!
+    
+    @IBOutlet weak var caloriesLabel: UILabel!
+    
+    @IBOutlet weak var textLabel: UILabel!
+    let colors:[UIColor] = [.red, .orange,.yellow,.green]
+    let calories = [33,45,105,95]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        if sender.tag == 1000{
+            caloriesLabel.text = ""
+        }
+        else {
+            print(sender.tag)
+            print(sender.titleLabel!.text!)
+            caloriesLabel.textColor = colors[sender.tag]
+            caloriesLabel.text = String(calories[sender.tag])+" Calories"
+        }
+        
+    }
+    
+    @IBAction func enterButtonPressed(_ sender: UIButton) {
+        textLabel.text = "Hi \(inputTextField.text!) Welcome to my App"
+    }
 }
 
